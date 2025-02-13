b1b30896f7bea1c14ab146da5e56b2bf52093307
Joey Cadieux <instructor.joeycadieux@gmail.com>
2025-02-13 10:10:29 -0500

    Refactor App component and add Header component

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+      return (
+            <View style={styles.container}>
+                  <StatusBar
+                        backgroundColor="transparent"
+                        barStyle="light-content"
+                        translucent={true}
+                  />
+                  <View style={styles.header}>
+                        <Text style={styles.headerText}>Header</Text>
+                  </View>
+                  <View style={styles.content}>
+                        <Text>Welcome to the app!</Text>
+                  </View>
+            </View>
+      );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      container: {
+            flex: 1,
+            backgroundColor: '#afb3b0',
+      },
+      header: {
+            height: 90,
+            backgroundColor: '#182e8f',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: StatusBar.currentHeight, // Adjust for the status bar height
+      },
+      headerText: {
+            color: 'white',
+            fontSize: 20,
+      },
+      content: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+      },
 });
+
+export default App;
